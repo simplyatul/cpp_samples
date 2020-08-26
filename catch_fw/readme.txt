4a39dc924084d7389ada7732d5f7c2ef318a6868
simplyatul <atulthosar@gmail.com>
2020-08-26 16:48:00 +0530

    reorg for cmake...

---
+++ catch_fw/readme.txt
@@ -0,0 +1,3 @@
+
+- Fire cmake in build dir only. o/w it will little up the directory
+
--- catch_fw/src/main.cpp
+++ catch_fw/src/main.cpp
@@ -5,7 +5,8 @@ int main()
 {
  // your code goes here
  int a,b,c;
+ std::cout<< "Enter 3 digits" << std::endl;
  std::cin>>a>>b>>c;
- std::cout<<GreatestOfThree(a,b,c)<< std::endl;
+ std::cout<<"Greatest Of Three is: " << GreatestOfThree(a,b,c)<< std::endl;
  return 0;
 }
