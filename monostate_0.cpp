00a64655c0e3f1dae1e6401dbb38ec24a8b7eef7
simplyatul <atulthosar@gmail.com>
2022-06-20 14:49:23 +0530

    Update monostate_0.cpp

--- monostate_0.cpp
+++ monostate_0.cpp
@@ -2,4 +2,8 @@
  * run: g++ monostate_0.cpp
  * monostate pattern imposes singularity thought behavior whereas
  * singleton pattern imposes singularity thought structure 
+ *
+ * In monostate pattern, all members (methods and variables) of a class
+ * are declared as static. This creates a single state without having to
+ * create an instance of a class. 
  */
