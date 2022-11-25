86911ca2f0afadbed898f44294297817c8e1d999
simplyatul <atulthosar@gmail.com>
2022-11-25 16:05:43 +0530

    Update singleton_0.cpp

--- singleton_0.cpp
+++ singleton_0.cpp
@@ -15,6 +15,15 @@ private:
     //Logger(const Logger&) = delete ;
     //Logger & operator=(const Logger&) = delete ;
 public:
+    Logger(const Logger&) {
+        std::cout << "In copy c'tor" << std::endl;
+    }
+
+    Logger & operator=(const Logger& l) {
+         std::cout << "In assignment operator" << std::endl;
+         return *this;
+    }
+
     static Logger & Instance() {
         static Logger m_instance;
         return m_instance;
@@ -29,20 +38,46 @@ public:
 
 int main()
 {
+    std::cout << "lg object " << std::endl;
     Logger lg = Logger::Instance();
     lg.log(); // prints different address
+    std::cout << std::endl;
+
+    std::cout << "lg1 object " << std::endl;
     Logger lg1 = Logger::Instance();
     lg1.log(); // prints different address
+    std::cout << std::endl;
+
+    std::cout << "lg2 object " << std::endl;
     Logger lg2 = lg1;
     lg2.log(); // prints different address
+    std::cout << std::endl;
+
+    std::cout << "assignment" << std::endl;
+    lg2 = lg1;
     return 0;
 }
 
+
 /*
  * Output
-    In log: 0x7ffcdf72eba5
-    In log: 0x7ffcdf72eba6
-    In log: 0x7ffcdf72eba7
- *
- */
+ 
+ 
+lg object 
+In copy c'tor
+In log: 0x7ffcd273c5ef
+
+lg1 object 
+In copy c'tor
+In log: 0x7ffcd273c5ee
+
+lg2 object 
+In copy c'tor
+In log: 0x7ffcd273c5ed
+
+assignment
+In assignment operator
+
+*
+*/
 
