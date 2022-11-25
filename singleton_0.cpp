fd4428a652fc06e68f90d487c4b3fc99900b712d
simplyatul <atulthosar@gmail.com>
2022-11-25 16:30:28 +0530

    Update singleton_0.cpp

--- singleton_0.cpp
+++ singleton_0.cpp
@@ -11,12 +11,18 @@
 class Logger
 {
 private:
-    Logger() = default;
+    Logger() {
+        std::cout << "In c'tor" << std::endl;
+    }
     //Logger(const Logger&) = delete ;
     //Logger & operator=(const Logger&) = delete ;
 public:
-    Logger(const Logger&) {
+    Logger(Logger&) {
         std::cout << "In copy c'tor" << std::endl;
+    }
+
+    Logger(const Logger&) {
+        std::cout << "In const copy c'tor" << std::endl;
     }
 
     Logger & operator=(const Logger& l) {
@@ -26,10 +32,11 @@ public:
 
     static Logger & Instance() {
         static Logger m_instance;
+        std::cout << "Static object: " << &m_instance << std::endl;
         return m_instance;
     }
 
-    void log() {
+    void log() const {
         std::cout << "In log: " << this << std::endl;
     }
 
@@ -44,7 +51,7 @@ int main()
     std::cout << std::endl;
 
     std::cout << "lg1 object " << std::endl;
-    Logger lg1 = Logger::Instance();
+    const Logger lg1 = Logger::Instance();
     lg1.log(); // prints different address
     std::cout << std::endl;
 
@@ -55,28 +62,39 @@ int main()
 
     std::cout << "assignment" << std::endl;
     lg2 = lg1;
+    std::cout << std::endl;
+
+    std::cout << "Pointer" << std::endl;
+    Logger *ptr = &Logger::Instance();
+    Logger lg3 = *ptr;
     return 0;
 }
 
 
 /*
  * Output
- 
- 
+  
 lg object 
+In c'tor
+Static object: 0x4041d2
 In copy c'tor
-In log: 0x7ffcd273c5ef
+In log: 0x7fff84b73277
 
 lg1 object 
+Static object: 0x4041d2
 In copy c'tor
-In log: 0x7ffcd273c5ee
+In log: 0x7fff84b73276
 
 lg2 object 
-In copy c'tor
-In log: 0x7ffcd273c5ed
+In const copy c'tor
+In log: 0x7fff84b73275
 
 assignment
 In assignment operator
+
+Pointer
+Static object: 0x4041d2
+In copy c'tor
 
 *
 */
