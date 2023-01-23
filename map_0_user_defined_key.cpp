668f651bf8c72ce479e7021c458c767745664a64
simplyatul <atulthosar@gmail.com>
2023-01-23 18:46:27 +0530

    Create map_0_user_defined_key.cpp

---
+++ map_0_user_defined_key.cpp
@@ -0,0 +1,69 @@
+/*
+ * run: g++ map_0_user_defined_key.cpp --std=c++17
+ * Demonstrate how to use User defined class as a Key of a map 
+ */
+ 
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int id;
+public:
+    Student(string n, int i): name(n), id(i) {}
+    string getName() const {
+        return name;
+    }
+    int getId() const {
+        return id;
+    }
+    
+    bool operator <(const Student& s) const {
+        if(s.name > this->name) return true;
+        return false;
+    }
+};
+
+struct StudentIdComparator {
+    bool operator() (const Student &left, const Student &right) const {
+        if(left.getId() < right.getId()) return true;
+        return false;
+    }
+};
+
+void insertIntoMap(std::map<Student, int> &s) {
+    s.insert(std::pair<Student, int>(Student("Ram", 20), 20));
+    s.insert(std::pair<Student, int>(Student("Sham", 50), 50));
+    s.insert(std::pair<Student, int>(Student("Kiran", 5), 5));
+}
+
+void insertIntoMapWithComparator(std::map<Student, int, StudentIdComparator> &s) {
+    s.insert(std::pair<Student, int>(Student("Ram", 20), 20));
+    s.insert(std::pair<Student, int>(Student("Sham", 50), 50));
+    s.insert(std::pair<Student, int>(Student("Kiran", 500), 5));
+}
+
+
+int main()
+{
+    std::map<Student, int> s1Map;
+    insertIntoMap(s1Map);
+    
+    cout << "Sorted by Name using operator <" << endl;
+    for (const auto &it: s1Map) {
+        cout << it.first.getName() << " " << it.first.getId() << endl;
+        //cout << it.first.getName() << endl;
+    }
+    
+    std::map<Student, int, StudentIdComparator> s2Map;
+    insertIntoMapWithComparator(s2Map);
+    
+    cout << "Sorted by Id using comparator operator ()" << endl;
+    for (const auto &it: s2Map) {
+        cout << it.first.getName() << " " << it.first.getId() << endl;
+    }
+}
