95426b07b03bb7d174802a36f9a58992fdc6c2c6
Atul Thosar <atulthosar@gmail.com>
2024-03-04 18:54:40 +0530

    Create fscanf_0.c

---
+++ fscanf_0.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * cat version.txt
+ * VERSION=002.024.000
+ *
+ */
+
+#define LEN 3
+char vProfName[LEN] = {0};
+
+int main() {
+
+    FILE *fp = fopen("/tmp/" "version.txt", "r");
+    if ( NULL == fp)
+    {
+        printf("Error: %s\n", "file not present\n");
+        return -1;
+    }
+
+    char format[32]={0};
+    snprintf(format, sizeof(format), "%%%ds", (int)(LEN-1));
+    printf("format: %s\n", format);
+
+    fscanf(fp, format, vProfName); // avoids buffer overflow
+    printf("vProfName: %s\n", vProfName);
+    fclose(fp);
+}
