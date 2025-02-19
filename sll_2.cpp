d97711154448b0648139e87b230c9dd577d8aa5f
Atul Thosar <atulthosar@gmail.com>
2025-02-19 22:34:08 +0530

    update 3

--- sll_2.cpp
+++ sll_2.cpp
@@ -8,63 +8,70 @@
  * Given the head of a sorted linked list, delete all nodes that have duplicate 
  * numbers, leaving only distinct numbers from the original list.
  * Return the linked list sorted as well.
+ * 
+ * Input 1 2 2 3
+ * Output 1 3
+ * 
+ * Ref: https://walkccc.me/LeetCode/problems/82/#__tabbed_1_1
 */
 
 using namespace std;
 
 class Solution {
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        /*
-         * Not working....
-        */
-        if(!head) return nullptr;
-        ListNode* curr = head;
-        ListNode* start = nullptr;
-        ListNode* end = nullptr;
-        ListNode* prev = head;
-        bool skip = false;
-        while(nullptr != curr && curr->next) {
-            if(curr->val == curr->next->val) {
-                if(!start) {
-                    start = curr;
-                    // find end
-                    while(nullptr != curr && start->val == curr->val) {
-                        end = curr;
-                        curr = curr->next;
-                        skip=true;
-                    }
-                    // point prev-next to curr
-                    if(prev == start) {
-                        head = curr;
-                    } else {
-                        prev->next = curr;
-                    }
-                    start = nullptr;
-                }
+    // Q: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+    // deletes all duplicates except one
+    ListNode *deleteDuplicatesOne(ListNode *head) {
+        ListNode dummy(0, head);
+        ListNode *prev = head;
+        bool movePrev = true;
+        while (head != nullptr) {
+            while (head->next && head->val == head->next->val) {
+                head = head->next;
             }
-            prev = curr;
-            if(skip) {
-                skip=false;
-                continue;
+            if(head != prev) {
+                prev->next = head->next;
+                prev = head->next;
             }
-            if(nullptr != curr)
-                curr = curr->next;
+            head = head->next;
+            prev = head;
         }
-        return head;
+        return dummy.next;
+    }
+
+    // deletes all duplicates
+    ListNode *deleteDuplicatesTwo(ListNode *head)
+    {
+        ListNode dummy(0, head);
+        ListNode *prev = &dummy;
+
+        while (head != nullptr)
+        {
+            while (head->next && head->val == head->next->val)
+                head = head->next;
+            if (prev->next == head)
+                prev = prev->next;
+            else
+                prev->next = head->next;
+            head = head->next;
+        }
+
+        return dummy.next;
     }
 };
 
 int main() {
     int n = 2;
     vector < vector<int> > input = {
+        {1,2,2,3},
+        {1,1,2},
         {4, 4, 6, 6, 6, 8, 9, 9, 10, 10},
         {4 ,4, 6, 6},
         {4,4},
         {1,2,2},
-        {1,1,2},
         //{1,2,3,4,5}
-        {1,2,2,3}
+        {1,2,2,3},
+        {-1, 0, 1,2,3,4}
     };
     Solution s;
     for(auto i: input) {
@@ -72,7 +79,7 @@ int main() {
         cout << "In SLL:  ";
         printSLL(head);
         cout << endl;
-        head = s.deleteDuplicates(head);
+        head = s.deleteDuplicatesOne(head);
         cout << "Out SLL: ";
         printSLL(head);
         cout << endl;
